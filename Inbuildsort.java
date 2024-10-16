607206a060046991d5a311c25743c6321a0ef4e6
Vivek Pawar <112761534+vsp1031@users.noreply.github.com>
2024-10-16 09:04:23 +0530

    Create Inbuildsort.java

---
+++ Inbuildsort.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class ArraysSort {
+    public static void main(String[] args) {
+        int[] data = {5, 2, 8, 3, 1};
+        Arrays.sort(data);
+        System.out.println("Sorted array: " + Arrays.toString(data));
+    }
+}
